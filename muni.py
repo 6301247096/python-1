bbe4b6cb18a3f1a31b49e84e620b7b19038bc5ea
chilaka bala muneendra <munichilaka291@gmail.com>
2023-04-25 14:55:33 +0530

    hi chilaka

---
+++ muni.py
@@ -0,0 +1,13 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('hello,world!')
+hello,world!
+
+=========================== RESTART: C:/Users/Administrator/Desktop/New folder (5)/new.py ===========================
+hello, world! 
+
+=========================== RESTART: C:/Users/Administrator/Desktop/New folder (5)/new.py ===========================
+hello, world! 
+
+=========================== RESTART: C:/Users/Administrator/Desktop/New folder (5)/new.py ===========================
+hello, world! 
